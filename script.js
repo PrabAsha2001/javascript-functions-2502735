f36f02d5eadca891fdc47b9919bf0c5ea191f488
PrabAsha2001 <umiasha79@gmail.com>
2023-09-28 15:39:17 +0530

    01.01

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-/*async function output(data) {
-    const $ = document.querySelector.bind(document)
-    let output = '<nav><ul>'
-    for (const key in data) {
-        output +=`<li><a class="bi-${key}" href="${data[key]}"> ${key}</a>`
-    }
-    output +='</ul></nav>'
-    $('#output').innerHTML = output
-}
 
-async function getSocial() {
-    const res = await fetch('./social.json')
-    const data = await res.json()
-    return data
-}
-
-async function doSocial() {
-    const social = await getSocial()
-    await output(social)
-}
-
-doSocial();*/
 const $=document.querySelector.bind(document);
 
 
